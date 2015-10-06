c5503b586b2c6f0ade0d0e6470d9071b54966b0f
Alain Metge <alain@macalain.home>
2015-10-06 18:53:37 +0200

    add vnext build step

--- build.cmd
+++ build.cmd
@@ -4,8 +4,10 @@ if NOT ERRORLEVEL 0 EXIT /B 1
 
 IF '%Configuration%' == '' (
   CMD /C dnu pack src\Jellyfish.Configuration --configuration Release
+  CMD /C dnu pack src\Jellyfish.Configuration.vnext --configuration Release
 ) ELSE (
   CMD /C dnu pack src\Jellyfish.Configuration --configuration %Configuration%
+  CMD /C dnu pack src\Jellyfish.Configuration.vnext --configuration %Configuration%
 )
 if NOT ERRORLEVEL 0 EXIT /B 1
 
